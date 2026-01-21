64e6e0d9769ac7e88d036f01d44dbf944136ca3d
cuinixam <22344617+cuinixam@users.noreply.github.com>
2026-01-21 21:19:34 +0100

    feat: add --help optionfor the pc terminal executable

--- platforms/pc_terminal/src/main.c
+++ platforms/pc_terminal/src/main.c
@@ -1,9 +1,31 @@
 #include "autoconf.h"
 #include "os.h"
+#include <stdio.h>
+#include <string.h>
 
-int main(void) {
+static void print_help(void) {
+  printf("SPLed PC Terminal\n");
+  printf("\n");
+  printf("Keyboard Commands:\n");
+  printf("  Press 'P' to power ON. Press it again to power OFF.\n");
+#if CONFIG_BLINKING
+  printf("  Arrow Up/Down control the blinking frequency.\n");
+#elif defined(CONFIG_BRIGHTNESS_ADJUSTMENT_MANUAL) &&                          \
+    CONFIG_BRIGHTNESS_ADJUSTMENT_MANUAL == 1
+  printf("  Arrow Up/Down control the brightness of the light.\n");
+#elif defined(CONFIG_BRIGHTNESS_ADJUSTMENT_AUTOMATIC) &&                       \
+    CONFIG_BRIGHTNESS_ADJUSTMENT_AUTOMATIC == 1
+  printf("  Arrow Up/Down have no effect.\n");
+#endif
+}
 
-    OsRun();
-
+int main(int argc, char *argv[]) {
+  if (argc > 1 && strcmp(argv[1], "--help") == 0) {
+    print_help();
     return 0;
+  }
+
+  OsRun();
+
+  return 0;
 }
